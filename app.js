efdcc7d8bac5a2a81fa1b5e78c2acead69ed1681
Maximilian C Kwok <max.kwok@gmail.com>
2018-01-27 11:55:14 -0800

    Committing last save

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // TODO
+var App = () => (
+  <div> Hello World </div>
+);
+
+var Cucumbers = () => (
+  <li>Cucumbers</li>
+);
+
+var Kale = () => (
+  <li>Kale</li>
+);
+
+class GroceryListItem = (item) => {
+  {console.log(item);}
+  return (
+    <li>{item}</li>
+  )
+}
+
+var GroceryList = (groceryItems) => {
+
+  var onListItemClick = (event) => {
+    console.log('I got clicked');
+  };
+
+  return(
+    <ul>
+      <h2>Grocery List</h2>
+      {groceryItems.items.map(function(item){
+        console.log(item);
+        return <GroceryListItem todo={item}/>;
+      })};
+    </ul>
+  );
+}
+
+ReactDOM.render(<GroceryList items={['Tomatoes', 'Mangoes', 'Grapes']}/>, document.getElementById("app"));
